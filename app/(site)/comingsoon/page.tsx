a3ce11b40f0fa4eba03a13e09077eaa0502d10cb
Trung Văn <74370274+VanTrung0307@users.noreply.github.com>
2023-10-27 19:37:05 +0700

    turn off metadata in coming soon

--- app/(site)/comingsoon/page.tsx
+++ app/(site)/comingsoon/page.tsx
@@ -1,10 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Coming Soon',
-}
+import { useEffect, useState } from 'react'
 
 const ComingSoonPage = () => {
   const styles = {
