bee852648faabdb79a6fbead1467a21bd683475b
Trung Văn <74370274+VanTrung0307@users.noreply.github.com>
2024-02-02 10:32:18 +0700

    change import

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from 'next-themes'
 const inter = Inter({ subsets: ['latin'] })
 import '../globals.css'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import ToasterContext from '../context/ToastContext'
 
